6867a084a79e833544b60e33a2f7a01242660a3d
Friedrich Schöller <code@schoeller.se>
2017-06-24 00:15:36 +0200

    Improve store

--- weight_system_store_set.cpp
+++ weight_system_store_set.cpp
@@ -2,15 +2,36 @@
 
 #include <set>
 
-struct WeightSystem {
+class WeightSystem {
     int16_t weights[DIMENSION];
 
+public:
     bool operator<(const WeightSystem &rhs) const
     {
         for (size_t i = DIMENSION; i--;)
             if (weights[i] != rhs.weights[i])
                 return weights[i] < rhs.weights[i];
         return false;
+    }
+
+    WeightSystem(const Equation &eq)
+    {
+        for (size_t i = 0; i < DIMENSION; ++i)
+            weights[i] = eq.a[i];
+    }
+
+    operator Equation() const
+    {
+        Equation eq;
+
+        eq.c = 0;
+        for (size_t i = 0; i < DIMENSION; ++i) {
+            eq.a[i] = weights[i];
+            eq.c -= eq.a[i];
+        }
+        eq.c = eq.c * 2 / TWO_TIMES_R;
+
+        return eq;
     }
 };
 
@@ -34,11 +55,7 @@ void weight_system_store_free(weight_system_store_t *store)
 
 void weight_system_store_insert(weight_system_store_t *store, const Equation *e)
 {
-    WeightSystem ws;
-    for (size_t i = 0; i < DIMENSION; ++i)
-        ws.weights[i] = e->a[i];
-
-    store->s.insert(ws);
+    store->s.insert(WeightSystem{*e});
 }
 
 int weight_system_store_size(weight_system_store_t *store)
@@ -57,13 +74,7 @@ const Equation *weight_system_store_next(weight_system_store_t *store)
         return nullptr;
 
     const WeightSystem &ws = *store->it++;
-
-    store->eq.c = 0;
-    for (size_t i = 0; i < DIMENSION; ++i) {
-        store->eq.a[i] = ws.weights[i];
-        store->eq.c -= store->eq.a[i];
-    }
-    store->eq.c = store->eq.c * 2 / TWO_TIMES_R;
+    store->eq = ws;
 
     return &store->eq;
 }
