fc7463519c8dd7551c089ebcf3468ac6a2dd8c73
Friedrich Schöller <code@schoeller.se>
2017-07-11 00:36:00 +0200

    Change note

--- weight_system_builder.h
+++ weight_system_builder.h
@@ -17,7 +17,7 @@ public:
 
     unsigned generator_count() const;
 
-    // the indices in the pairs are in ascending orders
+    // The indices appear in ascending orders
     std::vector<std::pair<unsigned, unsigned>> symmetries() const;
     bool has_symmetry(unsigned idx1, unsigned idx2) const;
 
