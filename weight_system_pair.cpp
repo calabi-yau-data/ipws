079b338b387dae52bf4124b8121a6aa15bb12d00
Friedrich Schöller <code@schoeller.se>
2017-07-12 16:31:26 +0200

    Fix pair canonicalization

--- weight_system_pair.cpp
+++ weight_system_pair.cpp
@@ -20,24 +20,10 @@ WeightSystemPair canonicalize(const WeightSystemPair &pair)
         option1[i][0] = pair.second.weights[i];
     }
 
-    auto cmp = [](auto &a, auto &b) {
-        if (a[0] != b[0])
-            return a[0] > b[0];
-        return a[1] > b[1];
-    };
+    std::sort(option0.begin(), option0.end());
+    std::sort(option1.begin(), option1.end());
 
-    std::sort(option0.begin(), option0.end(), cmp);
-    std::sort(option1.begin(), option1.end(), cmp);
-
-    bool choose_option0 = false;
-    for (unsigned i = 0; i < dim; ++i) {
-        if (cmp(option0[i], option1[i])) {
-            choose_option0 = true;
-            break;
-        }
-    }
-
-    auto &option = choose_option0 ? option0 : option1;
+    auto &option = option0 > option1 ? option0 : option1;
 
     for (unsigned i = 0; i < dim; ++i) {
         ret.first.weights[i] = option[i][0];
